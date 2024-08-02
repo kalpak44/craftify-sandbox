909b7fb89ff419862a147887f6539e3b77420a64
Pavel Ushanli <pau@foryouandyourcustomers.com>
2024-08-02 20:12:16 +0300

    Styling

--- frontend/craftify-app/src/pages/ProductsPage.jsx
+++ frontend/craftify-app/src/pages/ProductsPage.jsx
@@ -145,7 +145,7 @@ export const ProtectedPage = () => {
         <PageLayout>
             <div className="container mx-auto p-4">
                 <div className="flex justify-between items-center mb-4">
-                    <h1 className="text-xl font-bold text-white">Protected Page</h1>
+                    <h1 className="text-xl font-bold text-white">Products Page</h1>
                     <button
                         className="text-white font-bold py-2 px-4 rounded"
                         style={{
