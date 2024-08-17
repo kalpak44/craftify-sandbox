08f431d47aeac5859077b76f638afbb2a3ed0e35
Pavel Ushanli <pau@foryouandyourcustomers.com>
2024-08-17 23:22:44 +0300

    fix frontend ingress

--- craftify-app/src/services/API.js
+++ craftify-app/src/services/API.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:8080";
+const API_BASE_URL = "https://api.craftyfy.pro/";
 
 const fetchWithAuth = async (accessToken, url, options = {}) => {
     const headers = {
