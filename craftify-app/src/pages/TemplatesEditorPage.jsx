d9a6b220aafe52a02aeff02da1a3f3539cf8c182
Pavel Ushanli <pau@foryouandyourcustomers.com>
2025-07-03 19:33:31 +0300

    start playing with the templates-editor

--- craftify-app/src/pages/TemplatesEditorPage.jsx
+++ craftify-app/src/pages/TemplatesEditorPage.jsx
@@ -132,8 +132,8 @@ function OutputPlaceholderNode({ data }) {
             className="border-2 border-dashed border-gray-400 rounded-lg p-4 bg-gray-800 text-center cursor-pointer hover:bg-gray-700 transition"
             onClick={data.onClick}
         >
-            <div className="text-gray-300 font-medium text-lg">+ Add Output Node</div>
-            <div className="text-gray-500 text-xs mt-1">Click to create an output node</div>
+            <div className="text-gray-300 font-medium text-lg">+ Add Logical Node</div>
+            <div className="text-gray-500 text-xs mt-1">Click to create a logical node</div>
         </div>
     );
 }
