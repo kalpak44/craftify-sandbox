6f9de265a682bd83406a2d9067f94fbb5e1b94dc
Pavel Ushanli <pau@foryouandyourcustomers.com>
2025-07-19 17:17:05 +0300

    create simple event producer api

--- events-producer-api/src/main/java/com/craftify/producer/controller/EventProducerController.java
+++ events-producer-api/src/main/java/com/craftify/producer/controller/EventProducerController.java
@@ -19,8 +19,7 @@ public class EventProducerController {
   @PostMapping
   @Operation(summary = "Produce event", description = "Sends an event JSON to Kafka")
   public ResponseEntity<String> produceEvent(
-      @io.swagger.v3.oas.annotations.parameters
-              .RequestBody( // Fully qualified name for Swagger annotation
+      @io.swagger.v3.oas.annotations.parameters.RequestBody(
               description = "Example event JSON",
               required = true,
               content =
@@ -31,8 +30,7 @@ public class EventProducerController {
                               value =
                                   "{\"type\":\"EVENT_A\",\"payload\":{\"message\":\"Hello world!\"}}")))
           @org.springframework.web.bind.annotation.RequestBody
-          String eventJson // Fully qualified name for Spring annotation
-      ) {
+          String eventJson) {
     kafkaProducerService.sendMessage("event-topic", eventJson);
     return ResponseEntity.ok("Event sent to Kafka");
   }
