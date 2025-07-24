0381cb45013943cea195af15894fb92fe6908f00
Pavel Ushanli <pau@foryouandyourcustomers.com>
2025-07-24 14:48:04 +0300

    list form be

--- frontend/src/api/forms.js
+++ frontend/src/api/forms.js
@@ -1,5 +1,5 @@
 const API_HOST = import.meta.env.VITE_API_HOST || "http://localhost:8080";
-const FORMS_API_URL = `${API_HOST}/forms/`;
+const FORMS_API_URL = `${API_HOST}/forms`;
 
 
 /**
@@ -10,7 +10,7 @@ const FORMS_API_URL = `${API_HOST}/forms/`;
  * @returns {Promise<Object>} - The pageable response: { content, totalElements, totalPages, ... }
  */
 export async function listForms(authFetch, page = 0, size = 10) {
-    const url = new URL(`${FORMS_API_URL}`);
+    const url = new URL(`${FORMS_API_URL}/`);
     url.searchParams.append("page", page);
     url.searchParams.append("size", size);
 
